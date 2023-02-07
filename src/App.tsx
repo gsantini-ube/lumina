95461e3168fd2ba186004faddb4f2f0be7992ed5
gsantini-ube <gsantini@ubeswap.org>
2023-02-07 13:21:04 -0500

    added a loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import HoloImg from "./assets/backgrounds/holo.png";
+import SpinnerImg from "./assets/images/spinner.svg";
 
 function App() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = HoloImg;
+    image.onload = () => {
+      setImageLoaded(true);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <span className="title">Lumina</span>
-      <span className="subtitle">DEX</span>
-      <span className="description">
-        Fully compliant, enterprise-ready, decentralized exchange on Mina
-      </span>
+      {imageLoaded ? (
+        <>
+          <span className="title">Lumina</span>
+          <span className="subtitle">DEX</span>
+          <span className="description">
+            Fully compliant, enterprise-ready, decentralized exchange on Mina
+          </span>
+        </>
+      ) : (
+        <>
+          <img src={SpinnerImg} alt="" />
+        </>
+      )}
     </div>
   );
 }
