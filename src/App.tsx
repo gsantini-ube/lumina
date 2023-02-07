e8d2f69ed1b95ae2b9b74da3332cbe94fa1ac076
gsantini-ube <gsantini@ubeswap.org>
2023-02-07 09:43:35 -0500

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <span className="title">Lumina</span>
+      <span className="subtitle">DEX</span>
+      <span className="description">
+        Fully compliant, enterprise-ready, decentralized exchange on Mina
+      </span>
+    </div>
+  );
+}
+
+export default App;
